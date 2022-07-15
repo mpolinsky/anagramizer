e755030d1ad75861fc5cfbc1358ece146204f231
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-15 13:48:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,15 @@ word = st.text_input("Enter a word")
 
 st.write(word)
 
+def form_callback():
+    st.write(st.session_state.my_slider)
+    st.write(st.session_state.my_checkbox)
+
+with st.form(key='my_form'):
+    slider_input = st.slider('My slider', 0, 10, 5, key='my_slider')
+    checkbox_input = st.checkbox('Yes or No', key='my_checkbox')
+    submit_button = st.form_submit_button(label='Submit', on_click=form_callback)
+
 
 words = st.selectbox(label="Pick a word", options=['hello', 'world'], key = dt.now())
 
