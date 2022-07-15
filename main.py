feed6ccd2d8e0fb0c43ae0f40e8143bbf700f394
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-15 13:36:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def shrink_pool(current_name_counter, word_pool):
 def setup():
     name = st.text_input("Enter name: ").lower().strip().replace(' ','')
     pool = shrink_pool(Co(name), [i for i in ew.english_words_lower_alpha_set if len(i) > 4])
-    st.session_state.get(results=[])
+    st.session_state.get('results'=[])
     return name, pool
 
 
