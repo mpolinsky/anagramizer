3c4f7250ba4b108069f91e700d376e3f2ba7e7fb
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-15 12:51:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -37,14 +37,14 @@ word_pool = shrink_pool(counter, [i for i in ew.english_words_lower_alpha_set if
 next_word = st.selectbox(label="Choose next word", options=word_pool, key=str(dt.now()))
 # now the user has selected and stored a word.  We need to repeat this process until the pool returns empty.
 results = []
-while word_pool != []:
-    results.append(next_word)
-    st.write(next_word)
-    time.sleep(1)
-    counter = counter - Co(next_word)
-    word_pool = shrink_pool(counter, word_pool)
-    next_word = st.selectbox(label="Choose next word", options=word_pool, key=str(dt.now()))
-    
+#while word_pool != []:
+results.append(next_word)
+st.write(next_word)
+time.sleep(1)
+counter = counter - Co(next_word)
+word_pool = shrink_pool(counter, word_pool)
+next_word = st.selectbox(label="Choose next word", options=word_pool, key=str(dt.now()))
+
 st.write(results)
 st.subheader("Session State")
 st.write(list(st.session_state.keys()))
