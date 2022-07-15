58c1df28afd6ffbd65a55e6038586a899799896f
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-15 13:59:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,15 +23,16 @@ def shrink_pool(current_name_counter, word_pool):
     return newpool
 
 
-import streamlit as st
-
-st.title('Counter Example using Callbacks')
+st.title('Counter Example using Callbacks with args')
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
-def increment_counter():
-    st.session_state.count += 1
+increment_value = st.number_input('Enter a value', value=0, step=1)
 
-st.button('Increment', on_click=increment_counter)
+def increment_counter(increment_value):
+    st.session_state.count += increment_value
+
+increment = st.button('Increment', on_click=increment_counter,
+    args=(increment_value, ))
 
 st.write('Count = ', st.session_state.count)
