cc45e763dfbb4af9815f8dccd99e0993bb186fc6
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-15 12:45:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import english_words as ew
 from collections import Counter as Co
 import streamlit as st
 from datetime import datetime as dt
+import time
 
-
-st.cache()
+st.cache(allow_output_mutation=True)
 def get_name():
     name = st.text_input("Enter name: ").lower().strip().replace(' ','')
     return name, Co(name)
+
 
 # check for presence and number of letters to eliminate invalid words
 def letter_check(current_name_counter, candidate_word):
@@ -17,34 +18,35 @@ def letter_check(current_name_counter, candidate_word):
             return False
     return True
 
+
 # Keep words that pass the letter_check
 def shrink_pool(current_name_counter, word_pool):
     newpool = [i for i in word_pool if letter_check(current_name_counter, i)]
     newpool.sort(key=len, reverse=True)
     return newpool
 
-def reduce_round():
-    # declare streamlit state variables 
-    WORDLIMIT = 4
-    name, counter = get_name()
-    word_pool = shrink_pool(
-        counter,
-        [i for i in ew.english_words_lower_alpha_set if len(i) > 4]
-        )
-    return name, counter, word_pool
 
-st.header("Are we here?")
+st.subheader("Session State")
+st.write(list(st.session_state.keys()))
 
-def main():
-    name, counter, word_pool = reduce_round()
-    st.subheader("Session State")
-    st.write(list(st.session_state.keys()))
-    next_word = st.selectbox(label="Choose next word", options=word_pool, key=str(dt.now()))
+# Generate initial values name, the first name counter, and the initially reduced corpus
+name, counter = get_name()
+word_pool = shrink_pool(counter, [i for i in ew.english_words_lower_alpha_set if len(i) > 4])
+    
+# we want to tdisplay the pool tot he user for now in a selectbox list 
+next_word = st.selectbox(label="Choose next word", options=word_pool, key=str(dt.now()))
+# now the user has selected and stored a word.  We need to repeat this process until the pool returns empty.
+while word_pool != []:
     st.write(next_word)
-    return name, counter, word_pool
+    time.sleep(1)
+    counter = counter - Co(next_word)
+    word_pool = shrink_pool(counter, word_pool)
+    next_word = st.selectbox(label="Choose next word", options=word_pool, key=str(dt.now()))
+    
+st.subheader("Pool exhausted")
 
-name, counter, word_pool = main()
-
+st.subheader("Session State")
+st.write(list(st.session_state.keys()))
 
 st.subheader("End")
 
