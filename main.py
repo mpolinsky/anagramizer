69b170015c245e74b56fbde7c866ce4fdf84d696
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-15 12:10:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -33,38 +33,10 @@ word_pool = shrink_pool(
     )
 
 st.subheader("Session State")
-st.write(st.session_state.keys())
+st.write(list(st.session_state.keys()))
 
 
 
 
 
 
-
-"""
-def run(counter, wordpool):
-    # shrink pool
-    new_pool = shrink_pool(counter, wordpool)
-    with st.form(clear_on_submit=True, key=f'{datetime.now()}'):
-        # choose a word and collect word
-        word = st.selectbox(f'Make a Selection', new_pool)
-        submitted = st.form_submit_button('Submit')
-        if submitted:    
-            # adjust counter
-            new_counter = counter - Co(word)
-        return word, counter, new_pool
-
-
-def main():
-    results = list()
-    pool = [i for i in ew.english_words_lower_alpha_set if len(i) > WORDLIMIT]
-    name = st.text_input("Enter name: ").lower()
-    name = name.replace(' ','')
-    counter = Co(name)
-    word, counter, pool = run(counter, pool)
-    results.append(word)
-    if pool == []:
-        st.subheader(' '.join(results))
-        
-main()
-"""
