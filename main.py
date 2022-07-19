620e571c6cf3596f419d4cb85ea0df9fb89dab6b
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-19 13:40:38 -0400

    attempting to add form
    Adding an st.button without a form just broke the app.  I think it was not assigning option and not generating the list of new words.  I put the whole selectbox and option assignment in the form for now.  I believe this was a problem before.

--- main.py
+++ main.py
@@ -61,16 +61,19 @@ if st.session_state.count > 3:
 
 st.write('Count = ', st.session_state.count)
 
-option = st.selectbox(
-'Select:',
-shrink_pool(st.session_state.counter1, st.session_state.word_pool)
-)
-st.write(f"You chose {option}")
-submit = st.button("Submit")
-if submit:
-    st.session_state.choice = option	
-    st.session_state.res.append(st.session_state.choice)
-    st.subheader(st.session_state.counter1)
+with st.form(key=str(dt.now())):
+	option = st.selectbox(
+	'Select:',
+	shrink_pool(st.session_state.counter1, st.session_state.word_pool)
+	)
+	st.write(f"You chose {option}")
+
+
+	submit = st.form_submit_button("Submit")
+	if submit:
+	    st.session_state.choice = option	
+	    st.session_state.res.append(st.session_state.choice)
+	    st.subheader(st.session_state.counter1)
 
 if st.session_state.count > 1:
     st.session_state.counter1 -= Co(st.session_state.res[st.session_state.count])
