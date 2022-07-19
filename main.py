dc9a26bd0580a0903ad63bdb2a3715372c63868b
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-19 06:21:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -31,20 +31,23 @@ if 'results' not in st.session_state:
     name = st.text_input("Enter name: ").lower().replace(' ','')
     st.header(f"Name: {name}")
     st.session_state['name'] = name
-    st.session_state['counter'] = Co(name)
-    st.session_state['word_pool'] = shrink_pool(st.session_state.counter, [i for i in ew.english_words_lower_alpha_set if len(i) > 4])
+    #st.session_state['counter'] = Co(name)
+    #st.session_state['word_pool'] = shrink_pool(st.session_state.counter, [i for i in ew.english_words_lower_alpha_set if len(i) > 4])
     st.session_state.count = 0
     
-st.subheader(st.session_state['counter']) 
-st.subheader(st.session_state['word_pool']) 
+#st.subheader(st.session_state['counter']) 
+#st.subheader(st.session_state['word_pool']) 
 
-st.header(f"Session name is: {st.session_state['name']}")
+#st.header(f"Session name is: {st.session_state['name']}")
 #if st.session_state.results != []:
     #st.header(f"Session anagram is: {' '.join(st.session_state['results'])}")
 #else:
     #st.header(f"No anagram yet.")
 st.subheader(f"Word pool length is {len(st.session_state['word_pool'])}")
-st.session_state.choice = st.selectbox(label="Select word", options=st.session_state.word_pool, key=dt.now())
+c = st.selectbox(label="Select word", options=st.session_state.word_pool, key=dt.now())
+st.session_state.choice = c
+st.write(f"Choice is: {c}")
+st.write(f"Choice is {st.session_state.choice}")
 st.session_state.counter = st.session_state.counter - Co(st.session_state.choice)
 
 st.session_state.results.append(st.session_state.choice)
