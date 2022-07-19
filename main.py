0da01b9ee45976336e0926c688ce253b113aac52
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-19 13:28:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import english_words as ew
-from collections import Counter as Co
 import streamlit as st
+import english_words as ew
 from datetime import datetime as dt
-import time
+from collections import Counter as Co
+
+
 
 
 # check for presence and number of letters to eliminate invalid words
@@ -21,47 +22,66 @@ def shrink_pool(current_name_counter, word_pool):
     newpool = [i for i in word_pool if letter_check(current_name_counter, i)]
     newpool.sort(key=len, reverse=True)
     st.write(f"in shrink pool: returned pool size is {len(newpool)}")
+    #st.session_state.word_pool = newpool
     return newpool
 
-st.title('You can do it!')
 
+@st.cache(allow_output_mutation=True)
+def reset_counter(a_name):
+    st.session_state.counter1 = Co(st.session_state.name)
+
+
+
+# Streamlit runs from top to bottom on every iteraction so we check the state
+if 'word_pool' not in st.session_state:
+    st.session_state.word_pool = [i for i in ew.english_words_lower_alpha_set if len(i) > 3]
 
 if 'res' not in st.session_state:
-	st.session_state.res = list()
+    st.session_state.res = list()
 
-if 'word_pool' not in st.session_state:
-    st.session_state.word_pool = ['happy', 'delighted', 'glad']#[i for i in ew.english_words_lower_alpha_set if len(i) > 3]
-
+if 'choice' not in st.session_state:
+    st.session_state.choice = ''
 
 # If no, then initialize count to 0
 # If count is already initialized, don't do anything
 if 'count' not in st.session_state:
     st.session_state.count = 0
-    st.write("ONE TIME")
 	
-st.session_state.name = st.text_input('nameo: ')
-# Create a button which will increment the counter
-increment = st.button('Increment')
-if increment:
-    st.session_state.count += 1
+if 'name' not in st.session_state or st.session_state.name == "":
+    st.session_state.name = st.text_input("Enter name")
 
-# A button to decrement the counter
-decrement = st.button('Decrement')
-if decrement:
-    st.session_state.count -= 1
+if 'counter1' not in st.session_state:
+    st.session_state.counter1 = Co(st.session_state.name)
 
-st.session_state.choice = st.selectbox("Select a word", st.session_state.word_pool, key=str(dt.now()))
-st.session_state.counter_1 = Co(st.session_state.choice)
+reset_counter(st.session_state.name)
 
+
+if st.session_state.count > 3:
+    st.session_state.res = st.session_state.res[:-1]
 
 st.write('Count = ', st.session_state.count)
+
+option = st.selectbox(
+'Select:',
+shrink_pool(st.session_state.counter1, st.session_state.word_pool)
+)
+st.write(f"You chose {option}")
+    
+st.session_state.choice = option	
+st.session_state.res.append(st.session_state.choice)
+st.subheader(st.session_state.counter1)
+
+if st.session_state.count > 1:
+    st.session_state.counter1 -= Co(st.session_state.res[st.session_state.count])
+	
+st.subheader(st.session_state.counter1)
+	
+st.session_state.count += 1
+
+if st.session_state.counter1 == {}:
+    st.subheader(' '.join(st.session_state.res[2:]))
+
+st.button("Next")
+
 st.write(st.session_state)
-
-if st.session_state.count > 5:
-    st.session_state.res.append('red')
-
-st.write(st.session_state.res)
-
-
-
 
