4bf2a82004c6f4dfd66575b8123cf56c048b3ae5
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-19 14:57:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -73,8 +73,8 @@ with st.form(key=str(dt.now())):
 	    st.session_state.res.append(st.session_state.choice)
 	    st.subheader(st.session_state.counter1)
 
-if st.session_state.count > 2:
-	st.session_state.counter1 -= Co(st.session_state.res[st.session_state.count])
+if st.session_state.count > 1:
+	st.session_state.counter1 -= Co(st.session_state.res[:-1])
 	
 st.subheader(st.session_state.counter1)
 
