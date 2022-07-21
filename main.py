e707303eaa6d599b4107c4b44ab9fe3aca777f7b
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-20 20:51:45 -0400

    Adjusting
    have to adjust count index into res again.

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def turnabout(oparg):
     st.session_state.choice = oparg
     st.session_state.res.append(st.session_state.choice)
     st.header(f"st.session_state.choice is: {st.session_state.choice}")
-    st.session_state.counter1 -= Co(st.session_state.res[st.session_state.count-1])
+    st.session_state.counter1 -= Co(st.session_state.res[st.session_state.count-2])
     st.subheader(st.session_state.counter1)
 
 st.title("implement selectbox control")
