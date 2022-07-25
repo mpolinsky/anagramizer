50af2bab36be4c077ba5d64a7990eb2211539ea8
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-25 09:33:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -59,20 +59,21 @@ if 'reset' not in st.session_state:
 		
 reset_counter(st.session_state.name)
 
-if st.session_state.name != "":
-	st.header(f"Current anagram: {' '.join([i for i in st.session_state.res if i is not None])}")
-	st.header(f"Letters remaining: \n\t{''.join([ str(i)*st.session_state.counter1[i] for i in st.session_state.counter1 ]).replace('',' ')}")
-	st.session_state.word_pool = shrink_pool(st.session_state.counter1, st.session_state.word_pool)
-	st.session_state.word_pool.insert(0, None)
-	st.subheader("Select a word and click the select button to move on to the next word!")
-	selection = st.selectbox(
-	'Select:',
-	options = st.session_state.word_pool,
-	)
-	st.session_state.choice = selection
-	st.session_state.res.append(st.session_state.choice)
-	st.session_state.counter1 -= Co(st.session_state.res[st.session_state.count])
-	
+if st.session_state.name != "":	
+	if [i for i in st.session_state.word_pool if i is not None] != []:
+		st.header(f"Current anagram: {' '.join([i for i in st.session_state.res if i is not None])}")
+		st.header(f"Letters remaining: \n\t{''.join([ str(i)*st.session_state.counter1[i] for i in st.session_state.counter1 ]).replace('',' ')}")
+		st.session_state.word_pool = shrink_pool(st.session_state.counter1, st.session_state.word_pool)
+		st.session_state.word_pool.insert(0, None)
+		st.subheader("Select a word and click the select button to move on to the next word!")
+		selection = st.selectbox(
+		'Select:',
+		options = st.session_state.word_pool,
+		)
+		st.session_state.choice = selection
+		st.session_state.res.append(st.session_state.choice)
+		st.session_state.counter1 -= Co(st.session_state.res[st.session_state.count])
+
 	if [i for i in st.session_state.word_pool if i is not None] == []:
 		if st.session_state.counter1 == {}:
 			st.subheader(f"Congrats you found a true anagram for {st.session_state.name}!")
