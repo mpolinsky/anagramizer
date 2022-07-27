98454706efcb25ae36b291f3551f0d429670796f
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-27 14:49:20 -0400

    Oops last commit created a problem
    I was only removing whitespace in the case of a name containing digits or punctuation.  Had to move whitespace removal to cover all input.

--- main.py
+++ main.py
@@ -90,8 +90,8 @@ if 'name' not in st.session_state or st.session_state.name == "":
     st.session_state.og_name = st.text_input("Enter name")
     # If there are numbers or symbols don't save them in the og_name. Assume its a mistake.  Can change this if the corpus allows.
     if [i for i in st.session_state.og_name if i in string.punctuation + string.digits ] != []:
-        st.session_state.og_name = ''.join([i for i in st.session_state.og_name if i not in string.punctuation + string.whitespace + string.digits ])
-    st.session_state.name = st.session_state.og_name.lower()
+        st.session_state.og_name = ''.join([i for i in st.session_state.og_name if i not in string.punctuation + string.digits ])
+    st.session_state.name = ''.join([i for i in st.session_state.og_name.lower() if i not in string.whitespace])
 
 if 'user_anagram' not in st.session_state:
 	st.session_state.user_anagram = False
