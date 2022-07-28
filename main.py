d05975feaa186ba8a5f68ba8f64a1feec6d3bb32
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-28 14:56:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -163,11 +163,11 @@ if st.session_state.name != "":
 				st.subheader(f"""Choice: {st.session_state.choice}""")
 				st.experimental_rerun()
 	
-	if st.button("Start over"):
-		st.subheader("Starting fresh!")
-		st.session_state.clear()
-		reset_counter.clear()
-		st.experimental_rerun()		
+		if st.button("Start over"):
+			st.subheader("Starting fresh!")
+			st.session_state.clear()
+			reset_counter.clear()
+			st.experimental_rerun()		
 	else:
 		st.session_state.part2 = True
 	## Part 2
