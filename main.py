22b21ff7be22eef5e50bc7e1c3c94fadfd71da39
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-28 19:18:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ if st.session_state.name != "":
 				if letter_check(st.session_state.counter1, manual_entry):
 					selection = manual_entry
 				if not letter_check(st.session_state.counter1, manual_entry):
-					st.warning("Oops, that word isn't contained in the remaining letters.")
+					st.warning("Remember you can only use the remaining letters!")
 					selection = "Select a word!"
 			form_submit = st.form_submit_button("Select")
 			if form_submit:	
