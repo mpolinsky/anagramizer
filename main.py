ecca90d50c9b74c80b994f84750050d852a67767
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-07-28 18:39:55 -0400

    Adding letter_check to manual entry
    Have to check that a user-entered word is valid, i.e. that it passes letter check.  If it doesn't, the user is informed and null is added, harmlessly and they return to the same place.

--- main.py
+++ main.py
@@ -154,12 +154,13 @@ if st.session_state.name != "":
 			manual_entry = st.text_input("Or enter a word here!")
 			if manual_entry and manual_entry != "Select a word!":
 				st.write("Manual entry")
-				selection = manual_entry
+				if letter_check(st.session_state.counter1, manual_entry):
+					selection = manual_entry
+				else:
+					st.subheader("Oops, that word isn't contained in the remaining letters.")
+					selection = "Select a word!"
 			form_submit = st.form_submit_button("Select")
-			if form_submit:
-				if manual_entry:
-					# Check validity here
-					
+			if form_submit:	
 				st.session_state.choice = selection
 				if st.session_state.choice == "Select a word!":
 					st.session_state.res.append(None)
