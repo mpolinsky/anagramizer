c05c9322ccb8f52a82971a7395e2a36fb59af82f
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-02 14:06:59 -0400

    Added shrink_pool_creates_smaller_pools
    tests whether shrink_pool does indeed shrink pools

--- tests/test_letter_check.py
+++ tests/test_letter_check.py
@@ -1,5 +1,21 @@
 from collections import Counter as Co
+from tg_proj.letter_check import letter_check, shrink_pool
 
-def test_letter_check():
-  from tg_proj.letter_check import letter_check
+def test_letter_check_rejects_nonmatches():
   assert letter_check(Co('hat'), 'can') == False
+ 
+def test_letter_check_accepts_matches():
+  assert letter_check(Co('hat'), 'hat') == True
+
+def test_shrink_pool_creates_smaller_pools():
+  pool = [
+  'hat',
+  'at',
+  'a',
+  'sat',
+  'flat',
+  'mat',
+  'latter',
+  'madder'
+  ]
+  assert len(shrink_pool(Co(hat)))) < len(pool)
