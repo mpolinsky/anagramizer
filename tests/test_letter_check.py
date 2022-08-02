81f0c9d0b0d723c4a7b0b0e9d17584611f7395aa
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-02 14:28:54 -0400

    Added shrink_pool_returns_empty....

--- tests/test_letter_check.py
+++ tests/test_letter_check.py
@@ -1,21 +1,40 @@
 from collections import Counter as Co
 from tg_proj.letter_check import letter_check, shrink_pool
 
+@pytest.fixture()
+def pool():
+   pool = [
+    'hat',
+    'at',
+    'a',
+    'sat',
+    'flat',
+    'mat',
+    'latter',
+    'madder',
+    'get',
+    'but',
+    'mop',
+    'morph',
+    'prom',
+    'log',
+    'slog',
+    'good',
+    'top',
+    'last'
+  ]
+    return pool
+
 def test_letter_check_rejects_nonmatches():
   assert letter_check(Co('hat'), 'can') == False
  
 def test_letter_check_accepts_matches():
   assert letter_check(Co('hat'), 'hat') == True
 
-def test_shrink_pool_creates_smaller_pools():
-  pool = [
-  'hat',
-  'at',
-  'a',
-  'sat',
-  'flat',
-  'mat',
-  'latter',
-  'madder'
-  ]
+def test_shrink_pool_creates_smaller_pools(pool):
   assert len(shrink_pool(Co('hats'), pool)) < len(pool)
+
+def test_shrink_pool_returns_empty_for_no_matches(pool):
+  assert shrink_pool('git', pool) == []
+
+
