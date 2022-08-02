78d97741358852c496c05a6bdec39f9f368273f5
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-02 14:33:19 -0400

    added raises Attribute Error test and fixed another
    Was passing a string to shrink_pool should have passed a Counter.

--- tests/test_letter_check.py
+++ tests/test_letter_check.py
@@ -26,16 +26,23 @@ def pool():
    ]
    return pool
 
+# letter_check tests
+
 def test_letter_check_rejects_nonmatches():
   assert letter_check(Co('hat'), 'can') == False
  
 def test_letter_check_accepts_matches():
   assert letter_check(Co('hat'), 'hat') == True
 
+def test_string_passed_to_letter_check_raises_error():
+   assert letter_check('hat', 'hat') raises AttributeError
+   
+# shrink_pool tests   
+
 def test_shrink_pool_creates_smaller_pools(pool):
   assert len(shrink_pool(Co('hats'), pool)) < len(pool)
 
 def test_shrink_pool_returns_empty_for_no_matches(pool):
-  assert shrink_pool('git', pool) == []
+  assert shrink_pool(Co('git'), pool) == []
 
 
