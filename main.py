80a6215740c091ddbd280d14adbbce544dfec9bd
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-09 16:06:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -253,7 +253,7 @@ if st.session_state.name != "":
 					st.write("Note: If a Wikipedia search returns many results, the summary dislpayed here could be any of them.  Use the link to see the list!")	
 					st.session_state.info_render += 1
 			#tweet_data = st.session_state.anagram if st.session_state.user_anagram else ' '.join([i for i in st.session_state.res if i != "Select a word!"])
-			components.html("""<script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script><a href='https://twitter.com/share?text="HELLO WORLDS'" class="twitter-share-button" data-show-count="false">Tweet</a>""")
+			components.html("""<script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script><a href='https://twitter.com/share?text=st.session_state.anagram class="twitter-share-button" data-show-count="false">Tweet</a>""")
 
 		colA, colB, colC = st.columns([.25, 3.5, .25])
 		with colB:
