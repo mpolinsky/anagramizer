ff000685ec974bb928532b204f5dfa4b150f40e4
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-09-08 14:34:44 -0400

    Updated copyable code result
    It now has a complete phrase: "<anagram> is an anagram of <og_name>"

--- main.py
+++ main.py
@@ -234,9 +234,9 @@ if st.session_state.name != "":
 				st.session_state.anagram = st.text_input("If you see an anagram we've missed type it here!", value=None)
 				# Celebrate and display success message
 				if Co(st.session_state.anagram.lower().replace(' ','')) == Co(st.session_state.name):
-					st.subheader(f"You were right! {st.session_state.anagram} is an anagram for {st.session_state.og_name}")
+					st.subheader(f"You were right! {st.session_state.anagram} is an anagram of {st.session_state.og_name}")
 					st.subheader(f"  ")
-					st.code(f"{st.session_state.anagram}")
+					st.code(f"{st.session_state.anagram} is an anagram for {st.og_name)}!")
 					st.session_state.success = True
 					if st.session_state.balloons == 0:
 						st.balloons()
